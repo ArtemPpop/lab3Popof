466d46cb759359dc0c57deeb8d55e9e9ed90bc50
ArtemPpop <b.r.o.t.e.m.oo@gmail.com>
2024-02-20 00:44:28 +0200

    Добавьте файлы проекта.

---
+++ lab3Popof/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3Popof
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            double sideA = Convert.ToDouble(txtSideA.Text);
+            double sideB = Convert.ToDouble(txtSideB.Text);
+            double sideC = Convert.ToDouble(txtSideC.Text);
+            double angleA = Convert.ToDouble(txtAngleA.Text);
+            double angleB = Convert.ToDouble(txtAngleB.Text);
+            double angleC = Convert.ToDouble(txtAngleC.Text);
+
+            Triangle triangle = new Triangle(sideA, sideB, sideC, angleA, angleB, angleC);
+
+            double area = triangle.CalcArea();
+            double perimeter = triangle.CalcPerimetr();
+            double height = triangle.CalcHeight();
+            string type = triangle.TriangleType();
+
+            Result.Content = $"Площадь: {area}, Периметр: {perimeter}, Высота: {height}, Тип треугольника: {type}";
+        }
+    }
+}
---
+++ lab3Popof/Triangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3Popof
+{
+    internal class Triangle
+    {
+        public double SideA {  get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        public double AngleA { get; set; }
+        public double AngleB { get; set; }
+        public double AngleC { get; set; }
+
+        public Triangle(double sideA, double sideB, double sideC, double angleA, double angleB, double angleC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+            AngleA = angleA;
+            AngleB = angleB;
+            AngleC = angleC;
+        }
+        public double CalcArea()
+        {
+            double s=(SideA+SideB+SideC)/2;
+            return Math.Sqrt(s*(s-SideA)*(s-SideB) * (s - SideC));
+        }
+        public double CalcPerimetr()
+        {
+            return SideA+SideB+SideC;
+        }
+        public double CalcHeight()
+        {
+            double area =CalcArea();
+            return 2 * area / SideA;
+        }
+        public string TriangleType()
+        {
+            if (SideA == SideB && SideB == SideC)
+            {
+                return "Равносторонний треугольник";
+            }
+            else if (SideA == SideB || SideA == SideC || SideB == SideC)
+            {
+                return "Равнобедренный треугольник";
+
+            }
+            else if (Math.Round(AngleA) == 90 || Math.Round(AngleB) == 90 || Math.Round(AngleC) == 90)
+            {
+                return "Прямоугольный треугольник";
+            }
+            else 
+            {
+                return "Обычный треугольник";
+            }
+
+        }
+
+    }
+
+}
